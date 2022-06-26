a2cbf55da572acc1469039c1437448cf76af6121
Louie Munoz <munozlu.f@live.com>
2022-06-26 13:48:28 -0700

    added check month and day

---
+++ CheckMonthAndDay/CheckMonthAndDay.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace CheckMonthAndDay
+{
+    partial class CheckMonthAndDayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckMonthAndDayForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.monthLabel = new System.Windows.Forms.Label();
+            this.monthTextBox = new System.Windows.Forms.TextBox();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.dayTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.messageLabel, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 228);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(786, 228);
+            this.label4.TabIndex = 0;
+            this.label4.Text = resources.GetString("label4.Text");
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.Controls.Add(this.monthLabel);
+            this.flowLayoutPanel2.Controls.Add(this.monthTextBox);
+            this.flowLayoutPanel2.Controls.Add(this.dayLabel);
+            this.flowLayoutPanel2.Controls.Add(this.dayTextBox);
+            this.flowLayoutPanel2.Controls.Add(this.submitButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 254);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(794, 51);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // monthLabel
+            // 
+            this.monthLabel.AutoSize = true;
+            this.monthLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthLabel.Location = new System.Drawing.Point(3, 0);
+            this.monthLabel.Name = "monthLabel";
+            this.monthLabel.Size = new System.Drawing.Size(107, 51);
+            this.monthLabel.TabIndex = 3;
+            this.monthLabel.Text = "Month";
+            this.monthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // monthTextBox
+            // 
+            this.monthTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthTextBox.Location = new System.Drawing.Point(116, 3);
+            this.monthTextBox.Name = "monthTextBox";
+            this.monthTextBox.Size = new System.Drawing.Size(100, 45);
+            this.monthTextBox.TabIndex = 0;
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.AutoSize = true;
+            this.dayLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayLabel.Location = new System.Drawing.Point(222, 0);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(76, 51);
+            this.dayLabel.TabIndex = 4;
+            this.dayLabel.Text = "Day";
+            this.dayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dayTextBox
+            // 
+            this.dayTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayTextBox.Location = new System.Drawing.Point(304, 3);
+            this.dayTextBox.Name = "dayTextBox";
+            this.dayTextBox.Size = new System.Drawing.Size(100, 45);
+            this.dayTextBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(410, 3);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(146, 45);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageLabel.Location = new System.Drawing.Point(3, 354);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(794, 50);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "messageLabel";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.messageLabel.Visible = false;
+            // 
+            // CheckMonthAndDayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "CheckMonthAndDayForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check Month And Day";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label monthLabel;
+        private System.Windows.Forms.TextBox monthTextBox;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.TextBox dayTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ CheckMonthAndDay/CheckMonthAndDay.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckMonthAndDay
+{
+    /// <summary>
+    /// prompts a user to enter a birth month and day. Display an error message if the month is invalid (not  1 through 12) or the day is invalid for the month (for example, not between 1 and 31 for January or   between 1 and 29 for February). If the month and day are valid, display them with a message
+    /// </summary>
+    public partial class CheckMonthAndDayForm : Form
+    {
+
+        public CheckMonthAndDayForm()
+        {
+            InitializeComponent();
+        }
+        
+        enum Month
+        {
+            January = 1, Feburary, March, April, May, June, July, August, September, October, November, December
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            int month = Convert.ToInt16(monthTextBox.Text);
+            int day = Convert.ToInt16(dayTextBox.Text);
+            string message = string.Empty;
+
+            switch ((Month)month)
+            {
+                case Month.January:
+                    message = day > 0 && day < 31 ? $"{Month.January} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.Feburary:
+                    message = day > 0 && day < 29 ? $"{Month.Feburary} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.March:
+                    message = day > 0 && day < 31 ? $"{Month.March} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.April:
+                    message = day > 0 && day < 30 ? $"{Month.April} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.May:
+                    message = day > 0 && day < 31 ? $"{Month.May} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.June:
+                    message = day > 0 && day < 30 ? $"{Month.June} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.July:
+                    message = day > 0 && day < 31 ? $"{Month.July} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.August:
+                    message = day > 0 && day < 31 ? $"{Month.August} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.September:
+                    message = day > 0 && day < 30 ? $"{Month.September} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.October:
+                    message = day > 0 && day < 31 ? $"{Month.October} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.November:
+                    message = day > 0 && day < 30 ? $"{Month.November} {day} is valid" : "INCORRECT DAY";
+                    break;
+                case Month.December:
+                    message = day > 0 && day < 31 ? $"{Month.December} {day} is valid" : "INCORRECT DAY";
+                    break;
+                default:
+                    message = "INCORRECT MONTH VALUE";
+                    break;
+            }
+
+            messageLabel.Text = message;
+            messageLabel.Visible = true;
+
+            monthTextBox.SelectAll();
+            monthTextBox.Focus();
+            dayTextBox.Clear();
+        }
+
+    }
+}
